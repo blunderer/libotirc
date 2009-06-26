3e7007f7769edd9b8052f7720b7339cf45e26e66
Tristan Lelong <tristan@vlab.(none)>
2009-06-26 16:00:10 +0200

    correct slash for header comment

--- libotirc.c
+++ libotirc.c
@@ -1,3 +1,24 @@
+/** \file libotirc.c
+ *
+ * \author blunderer <blunderer@blunderer.org>
+ * \date 28 Jun 2009
+ *
+ * Copyright (C) 2009 blunderer
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #ifndef LIBOTIRC_C
 #define LIBOTIRC_C
@@ -228,7 +249,7 @@ void* irc_service(void * t)
 			}
 		}
 	}
-	return NULL;
+	return t;
 }
 
 
